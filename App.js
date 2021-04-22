cfd1e8d32cca2330b1f28f281b8889796383e21b
Adrian Kowalski <adriann.kowalski@gmail.com>
2021-04-22 11:21:45 +0200

    v 1.1
    - Add NavBar draft

--- App.js
+++ App.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import './App.css';
 import Products from './components/Products/Products'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
   <Products />
+  
     </div>
   );
 }
--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './Product/Product'
+import useStyles from './styles'
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes', price: '79 USD', image: 'https://50style.pl/media/cache/gallery/rc/ywdwd5sd/adidas-strutter-damskie-buty-lifestyle-bialy-fy8613.jpg'},
@@ -8,8 +9,10 @@ const products = [
 ]
 
 const Products = () =>{
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/> 
     <Grid container justify="center" spacing={4}>
       { products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
