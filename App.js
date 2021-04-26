ed8c4348e328a917c6bd80e68d0a0b2bce3ae112
Adrian Kowalski <adriann.kowalski@gmail.com>
2021-04-26 09:56:50 +0200

    v 1.5
    Buttons actions update:
    - increment and decrement cart items
    - remove the item from the cart
    - empty cart

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Products from './components/Products/Products'
 import NavBar from './components/NavBar/NavBar'
 import Cart from './components/Cart/Cart'
 import { commerce } from './lib/commerce'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   const [ products, setProducts ] = useState([])
@@ -21,27 +22,50 @@ function App() {
   }
 
   const handleAddToCart = async (productId, quantity) =>{
-    const item = await commerce.cart.add(productId, quantity)
-    setCart(item.cart)
+    const { cart } = await commerce.cart.add(productId, quantity)
+    setCart(cart)
   }
 
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity })
+    setCart(cart)
+  }
+
+  const handleRemoveFromCart = async ( productId ) => {
+    const { cart } = await commerce.cart.remove(productId)
+    setCart(cart)
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty()
+    setCart(cart)
+  }
 
   useEffect(() => {
   fetchProducts()
   fetchCart()
   }
   , [])
-
+ 
 
   return (
+    <Router>
     <div className="App">
       <NavBar totalItems={cart.total_items}/>
-     {/* <Products 
-      products={products}
-      onAddtoCart={handleAddToCart} 
-      /> */}
-    <Cart cart={cart}/>
+      <Switch>
+        <Route exact path='/'>
+          <Products products={products} onAddtoCart={handleAddToCart} /> 
+        </Route>
+        <Route exact path='/cart'>
+          <Cart cart={cart}
+           handleUpdateCartQty={handleUpdateCartQty}
+           handleRemoveFromCart={handleRemoveFromCart}
+           handleEmptyCart={handleEmptyCart}
+          />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
