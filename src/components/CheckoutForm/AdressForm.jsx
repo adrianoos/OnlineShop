e1e8ee9264f366c30fd25b1a162d165955991037
Adrian <adriann.kowalski@gmail.com>
2021-05-06 11:19:18 +0200

    form update added shipping countries, subdivisions, shipping options

--- src/components/CheckoutForm/AdressForm.jsx
+++ src/components/CheckoutForm/AdressForm.jsx
@@ -12,19 +12,43 @@ const AdressForm = ({ checkoutToken }) => {
     const [ shippingSubDivision, setShippingSubDivision ] = useState('')
     const [ shippingOptions, setShippingOptions ] = useState([])
     const [ shippingOption, setShippingOption ] = useState('')
+
     const methods = useForm()
     const countries = Object.entries(shippingCountries).map(([code, name]) => ({id: code, label: name}))
+    const subdivisions = Object.entries(shippingSubDivisions).map(([code, name]) => ({id: code, label: name}))
+    const options = shippingOptions.map((sOpt) => ({id: sOpt.id, label: `${sOpt.description} - (${sOpt.price.formatted_with_symbol})`}))
 
     const fetchShippingCountries = async (checkoutTokenId) => {
         const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId)
-        console.log(countries)
         setShippingCountries(countries)
         setShippingCountry(Object.keys(countries)[0])
+    };
+
+    const fetchSubdivisions = async (countryCode) => {
+        const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode)
+        setShippingSubDivisions(subdivisions)
+        setShippingSubDivision(Object.keys(subdivisions)[0])
+    };
+
+    const fetchShippingOptions = async ( checkoutTokenId, country, region = null) => {
+         const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region })
+        console.log(options)
+        setShippingOptions(options)
+        setShippingOption(options[0].id)
     };
 
     useEffect(() => {
         fetchShippingCountries(checkoutToken.id)
     }, [])
+
+    useEffect(() => {
+       if (shippingCountry) fetchSubdivisions(shippingCountry)
+    }, [shippingCountry])
+
+    useEffect(() => {
+      if (shippingSubDivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubDivision)
+    }, [shippingSubDivision])
+
     return (
         <>
             <Typography variant='h6' gutterBottom>Shipping Adress</Typography>
@@ -47,23 +71,27 @@ const AdressForm = ({ checkoutToken }) => {
                             ))}
                         </Select>
                     </Grid>
-                  {/*   <Grid item xs={12} sm={6}>
+                     <Grid item xs={12} sm={6}>
                         <InputLabel>Shipping SubDivision</InputLabel>
-                        <Select value={} fullWidth onChange={}>
-                            <MenuItem key={} value={}>
-                                Select ME
-                            </MenuItem>
+                        <Select value={shippingSubDivision} fullWidth onChange={(e) => setShippingSubDivision(e.target.value)}>
+                            { subdivisions.map((subdivision) => (
+                               <MenuItem key={subdivision.id} value={subdivision.id}>
+                                  {subdivision.label}
+                               </MenuItem>
+                            ))}
                         </Select>
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <InputLabel>Shipping Options</InputLabel>
-                        <Select value={} fullWidth onChange={}>
-                            <MenuItem key={} value={}>
-                                Select ME
-                            </MenuItem>
+                        <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
+                            { options.map((option) => (
+                               <MenuItem key={option.id} value={option.id}>
+                                  {option.label}
+                               </MenuItem>
+                            ))}
                         </Select>
-    </Grid> */}
                     </Grid>
+                 </Grid>
                 </form>
 
             </FormProvider>
